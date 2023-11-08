5fcaa895f512d06283544da020c0f80348e89b21
Leonardo Sampaio <leonardo.html@gmail.com>
2023-11-07 21:22:23 -0300

    rename project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center p-24">
-			<h1>Shadcn playground</h1>
+			<h1>Next.ts + Shadcn starter kit</h1>
 		</main>
 	);
 }
